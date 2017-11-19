66e106ce33faf31de9d9f981d4bb35d557f5f055
Matthieu <hello@matthieurebillard.fr>
2017-11-19 23:26:15 +0100

    Init the module

---
+++ dist/flow.js
@@ -0,0 +1,11 @@
+/**
+ * Initialisation d'une librairie d'exemple
+ * @type {{}}
+ */
+var Flow = Flow || {};
+Flow.init = function() {
+    console.log('Initialisation du module Flow')
+};
+(function() {
+    Flow.init();
+})();
